520c939630f5b09b9ef449682495f98c1f9bcbe7
tcyang <tcyang@gm.pu.edu.tw>
2021-04-21 11:19:19 +0800

    拍照後顯示圖片

--- app/src/main/java/tw/edu/pu/csim/tcyang/camerapermission/MainActivity.kt
+++ app/src/main/java/tw/edu/pu/csim/tcyang/camerapermission/MainActivity.kt
@@ -1,11 +1,15 @@
 package tw.edu.pu.csim.tcyang.camerapermission
 
+import android.content.ActivityNotFoundException
 import android.content.Intent
+import android.graphics.Bitmap
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
 import android.provider.Settings
 import android.view.View
+import android.widget.ImageView
 import android.widget.Toast
 import android.widget.Toast.*
 import com.karumi.dexter.Dexter
@@ -16,6 +20,9 @@ import com.karumi.dexter.listener.PermissionRequest
 import com.karumi.dexter.listener.single.PermissionListener
 
 class MainActivity : AppCompatActivity(), PermissionListener {
+
+    val REQUEST_IMAGE_CAPTURE = 1
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -30,6 +37,13 @@ class MainActivity : AppCompatActivity(), PermissionListener {
 
     override fun onPermissionGranted(p0: PermissionGrantedResponse?) {
         makeText(this, "您已允許拍照權限", LENGTH_SHORT).show()
+        
+        var it: Intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        try {
+            startActivityForResult(it, REQUEST_IMAGE_CAPTURE)
+        } catch (e: ActivityNotFoundException) {
+            // display error state to the user
+        }
     }
 
     override fun onPermissionDenied(p0: PermissionDeniedResponse?) {
@@ -48,4 +62,14 @@ class MainActivity : AppCompatActivity(), PermissionListener {
     override fun onPermissionRationaleShouldBeShown(p0: PermissionRequest?, p1: PermissionToken?) {
         p1?.continuePermissionRequest()
     }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            val imageBitmap = data?.extras?.get("data") as Bitmap
+            val img: ImageView = findViewById(R.id.img)
+            img.setImageBitmap(imageBitmap)
+        }
+    }
+
 }
